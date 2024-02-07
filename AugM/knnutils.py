abfbc4eac190267aa0090009dd8c1328fbb68b83
LiuPeiP-CS <80679851+LiuPeiP-CS@users.noreply.github.com>
2024-02-07 10:36:57 +0800

    Update knnutils.py

--- AugM/knnutils.py
+++ AugM/knnutils.py
@@ -9,28 +9,49 @@ import numpy as np
 import torch
 from torch.nn import functional as F
 
+def convert2begin0(labels, task_types_ids):
+    orig_type4id = dict()
+    for i, num in enumerate(task_types_ids):
+        orig_type4id[num] = i
+    new_labels = []
 
-def build_datastore(sup_ents_emb, sup_ents_labels, sup_ents_mask):
+    for lb in labels:
+        new_labels.append(orig_type4id[lb])
+    return np.array(new_labels)
+
+def build_datastore(sup_ents_emb, sup_ents_labels, sup_ents_mask, task_types_ids):
+    # 任务中原始的类标签
     # 面向test测试集的support支持集，构建{emb: ent_label}的数据库
     token_sum = sum(int(x.sum(dim=-1).cpu()) for x in sup_ents_mask)
     hidden_size = sup_ents_emb.shape[-1]
 
     data_store_key_in_memory = np.zeros((token_sum, hidden_size), dtype=np.float32)
-    data_store_val_in_memory = np.zeros((token_sum,), dtype=np.int32)
+    data_store_val_in_memory = np.zeros((token_sum,), dtype=np.int64)
 
     r_features = sup_ents_emb.reshape(-1, hidden_size) # (ent_num*sent*num, hid_dim)
     r_mask = sup_ents_mask.reshape(-1).bool()
     r_labels = sup_ents_labels.reshape(-1)
 
     labels = torch.masked_select(r_labels, r_mask).cpu().numpy() # 此处直接使用r_labels[r_mask].cpu().numpy()也可以实现
+    # print("The labels before converting is {}".format(labels))
+    labels = convert2begin0(labels, task_types_ids)
+    # print("The labels after converting is {}".format(labels))
     mask = r_mask.unsqueeze(1).repeat(1, hidden_size) # 同理可以由r_features[r_mask].view(-1, hidden_size).cpu()实现
     features = torch.masked_select(r_features, mask).view(-1, hidden_size).cpu()
-    np_features = features.numpy().astype(np.float32)
+    np_features = features.detach().numpy().astype(np.float32)
 
     data_store_key_in_memory[:] = np_features
     data_store_val_in_memory[:] = labels
 
     return {'keys': data_store_key_in_memory, 'vals': data_store_val_in_memory}
+
+
+def convert2tensor(ModelLogits):
+    # 输入的ModelLogits是一个数组的列表，将其转换成为tensor的形式
+    tensors = [torch.tensor(arr) for arr in ModelLogits]
+    result = torch.stack(tensors)
+    # result = torch.cat(tensors, dim=0)
+    return result
 
 def ModelAKNN4prob(sup_Datastore, testEntEmb, ModelLogits, args):
     # 此处为KNNNER的核心
@@ -39,10 +60,13 @@ def ModelAKNN4prob(sup_Datastore, testEntEmb, ModelLogits, args):
     link_temperature = torch.tensor(args.link_temperature).to(args.device)
     link_ratio = torch.tensor(args.link_ratio).to(args.device)
 
+    ModelLogits = convert2tensor(ModelLogits).to(args.device)
 
     """input logits should in the shape [seq_num, seq_len, num_labels]"""
     probabilities = F.softmax(ModelLogits, dim=2)  # shape of [seq_num, seq_len, num_labels]
     num_labels = probabilities.shape[-1]
+
+    # print("The shape of probabilities is {}".format(probabilities.shape))
 
     seq_num = testEntEmb.shape[0]
     max_ent_num = testEntEmb.shape[1]
@@ -65,13 +89,14 @@ def ModelAKNN4prob(sup_Datastore, testEntEmb, ModelLogits, args):
 
     sim_probs = torch.softmax(scores / link_temperature, dim=-1)  # [seq_num, max_ent_num, token_num]
 
-    knn_probabilities = torch.zeros_like(sim_probs[:, :, 0]).unsqueeze(-1).repeat([1, 1, num_labels])  # [seq_num, max_ent_num, num_labels]
+    knn_probabilities = torch.zeros_like(sim_probs[:, :, 0]).unsqueeze(-1).repeat([1, 1, num_labels]).to(args.device)  # [seq_num, max_ent_num, num_labels]
+    # print('The shape of knn_probabilities is {}'.format(knn_probabilities.shape))
     knn_probabilities = knn_probabilities.scatter_add(dim=2, index=knn_labels,
                                                       src=sim_probs)  # [seq_num, max_ent_num, num_labels]
-
+    # print('The shape of knn_probabilities is {}'.format(knn_probabilities.shape))
     probabilities = link_ratio * knn_probabilities + (1 - link_ratio) * probabilities
 
-    return probabilities
+    return probabilities.detach().cpu().numpy()
 
 
 
